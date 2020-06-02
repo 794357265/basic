89af37ac811987bc36e27baf9194a7110512399f
794357265@qq.com <xxx0.618>
2020-06-02 21:52:07 +0800

    整合mybatis

--- src/main/java/com/qianluohan/basic/controller/TypeController.java
+++ src/main/java/com/qianluohan/basic/controller/TypeController.java
@@ -1,14 +1,32 @@
 package com.qianluohan.basic.controller;
 
+import com.qianluohan.basic.entity.Item;
+import com.qianluohan.basic.service.TypeService;
 import com.qianluohan.basic.utils.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController("/type")
 public class TypeController {
 
+    @Autowired
+    private TypeService typeService;
+
     @GetMapping("/list")
     public Result getTypeList(){
+        return Result.ok();
+    }
+
+    public Result saveItems(){
+        List<Item> items = new ArrayList<>();
+        Item item = new Item();
+        item.setRemark("test");
+        items.add(item);
+        typeService.saveItems(items);
         return Result.ok();
     }
 
--- src/main/java/com/qianluohan/basic/dao/TypeDao.java
+++ src/main/java/com/qianluohan/basic/dao/TypeDao.java
@@ -1,6 +1,13 @@
 package com.qianluohan.basic.dao;
 
+import com.qianluohan.basic.entity.Item;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface TypeDao {
 
+    void saveItems(List<Item> items);
 
 }
